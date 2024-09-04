9069fa90c38db0a17ea725c5e042e3ac5f1cc3fb
Filipe Galvão <filipe.silva@sensedia.com>
2024-09-04 20:05:30 -0300

    [feat]: login in codewars platform

---
+++ login.js
@@ -0,0 +1,28 @@
+import puppeteer from "puppeteer";
+
+export async function login() {
+  const browser = await puppeteer.launch({
+    headless: false,
+    userDataDir: "session",
+  });
+
+  const page = await browser.newPage();
+  await page.setViewport({ 
+    width: 1200, 
+    height: 800,
+    deviceScaleFactor: 1,
+    isMobile: false 
+  });
+  
+  await page.goto("https://www.codewars.com/users/sign_in");
+  await page.waitForFunction(
+    'document.location.href == "https://www.codewars.com/dashboard"',
+    {
+      timeout: 3000000,
+    }
+  );
+
+  await browser.close();
+}
+
+login();
